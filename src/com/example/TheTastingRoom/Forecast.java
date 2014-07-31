34e7113d6ff422a03eabef584d7d7461e45a0847
Daniel Kamerling <dkamerling@riotgames.com>
2014-07-30 22:59:58 -0700

    Setup the initial welcome screen with transition to main game activity

---
+++ src/com/example/TheTastingRoom/Forecast.java
@@ -0,0 +1,18 @@
+package com.example.TheTastingRoom;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by dkamerling on 7/30/2014.
+ */
+    public class Forecast extends Activity {
+        /**
+         * Called when the activity is first created.
+         */
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.main);
+        }
+    }
--- src/com/example/TheTastingRoom/MyActivity.java
+++ src/com/example/TheTastingRoom/MyActivity.java
@@ -1,7 +1,12 @@
 package com.example.TheTastingRoom;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MyActivity extends Activity {
     /**
@@ -10,6 +15,11 @@ public class MyActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.landing);
+    }
+
+    public void startgame(View view) {
+        Intent intent = new Intent(this, Forecast.class);
+        startActivity(intent);
     }
 }
